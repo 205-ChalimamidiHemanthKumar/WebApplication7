a0e3b4c3b0f4d265f3c04e559990fbe3d5e36fc4
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-03 09:33:33 +0530

    Add project files.

---
+++ WebApplication7/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Account
+    {
+        public int ID { get; set; }
+        public string AccHolderName { get; set; }
+        public string AccHolderAddress { get; set; }
+    }
+}
---
+++ WebApplication7/Models/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Account> accounts = new List<Account>();
+            accounts.Add(new Account() { ID = 205, AccHolderName = "Hemanth", AccHolderAddress = "Tirupathi" });
+            accounts.Add(new Account() { ID = 206, AccHolderName = "Surya", AccHolderAddress = "Madanapalle" });
+            accounts.Add(new Account() { ID = 207, AccHolderName = "Rohit", AccHolderAddress = "Chittor" });
+            ViewData["accounts"] = accounts;
+            return View();
+        }
+    }
+}
